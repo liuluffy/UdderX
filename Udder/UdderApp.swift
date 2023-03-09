7904e824db572222030b657f0ddd07b6151688ea
15061883081@163.com <15061883081@163.com>
2023-03-09 21:24:13 +0800

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by 什么鬼 on 2023/3/9.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
